a8a57216c490cd0f9e08b0b8940954928c6114c6
Greg Reinacker <gregr@rassoc.com>
2014-02-19 16:46:47 -0700

    admin changes to work with Spree 2.0

--- app/models/zone_decorator.rb
+++ app/models/zone_decorator.rb
@@ -2,34 +2,55 @@
 # with added support for zipcode zone members
 Spree::Zone.class_eval do
 
-def kind
-  member = self.members.last
-  case member && member.zoneable_type
-  when "Spree::State"        then "state"
-  when "Spree::Zone"         then "zone"
-  when "Spree::Zipcode"      then "zipcode"
-  else
-    "country"
-  end
-end
+  attr_accessible :zipcode_ids
 
-
-# Check for whether an address.zipcode is available
-def include?(address)
-  return false unless address
-  
-  members.any? do |zone_member|
-    case zone_member.zoneable_type
-    when "Spree::Country"
-      zone_member.zoneable_id == address.country_id
-    when "Spree::State"
-      zone_member.zoneable_id == address.state_id
-    when "Spree::Zipcode"
-      zone_member.zoneable.name == address.zipcode
+  def kind
+    member = self.members.last
+    case member && member.zoneable_type
+    when "Spree::State"        then "state"
+    when "Spree::Zone"         then "zone"
+    when "Spree::Zipcode"      then "zipcode"
     else
-      false
+      "country"
     end
   end
-end
+
+
+  # Check for whether an address.zipcode is available
+  def include?(address)
+    return false unless address
+    
+    members.any? do |zone_member|
+      case zone_member.zoneable_type
+      when "Spree::Country"
+        zone_member.zoneable_id == address.country_id
+      when "Spree::State"
+        zone_member.zoneable_id == address.state_id
+      when "Spree::Zipcode"
+        zone_member.zoneable.name == address.zipcode
+      else
+        false
+      end
+    end
+  end
+
+  def zipcode_ids
+    if kind == 'zipcode'
+      members.collect(&:zoneable_id)
+    else
+      []
+    end
+  end
+
+  def zipcode_ids=(ids)
+    zone_members.destroy_all
+    ids.reject{ |id| id.blank? }.map do |id|
+      member = Spree::ZoneMember.new
+      member.zoneable_type = 'Spree::Zipcode'
+      member.zoneable_id = id
+      members << member
+    end
+  end
+
 
 end # Zone
